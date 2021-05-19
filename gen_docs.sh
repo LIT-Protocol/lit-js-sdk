7647d61d985aaac90e645a957045482a549df550
Chris Cassano <glitch0@gmail.com>
2021-05-18 19:38:16 -0700

    rename docs

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-documentation build src/** -f html --config documentation.yml -o docs_html
-documentation build src/** -f md --config documentation.yml -o docs_md
+documentation build src/** -f html --config documentation.yml -o api_docs_html
+documentation build src/** -f md --config documentation.yml -o api_docs.md
