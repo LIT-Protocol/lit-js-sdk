e2dcdce040aef7c6fd58e41a6298447734efeed5
Chris Cassano <glitch0@gmail.com>
2021-05-18 19:37:07 -0700

    added markdown docs

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
- documentation build src/** -f html --config documentation.yml -o docs
+documentation build src/** -f html --config documentation.yml -o docs_html
+documentation build src/** -f md --config documentation.yml -o docs_md
