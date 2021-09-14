6efc3128b6bf6b0ce9e862e618bfd652067b7f2f
Chris Cassano <glitch0@gmail.com>
2021-09-13 19:32:49 -0700

    fix versioning

--- publish.sh
+++ publish.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
 
-yarn build
-yarn buildWeb
 yarn versionBump
 RESULT=$(cat src/version.js | grep -E -o "'.*'")
 VERSION="${RESULT:1:${#RESULT}-2}" # strip the 's at the beginning and end
+
+yarn build
+yarn buildWeb
+
 yarn publish --new-version $VERSION
 
 echo "Don't forget to git push to get this running on the netlify cdn"
