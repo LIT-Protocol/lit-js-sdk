d606ec7cb6d91c5432a1cf9067c55bd3de7259fd
Chris Cassano <chris@litprotocol.com>
2022-01-10 15:54:04 -0500

    works well on server side with the esbuild polyfills added

--- esbuild-nodejs-shims.js
+++ esbuild-nodejs-shims.js
@@ -1,8 +1,14 @@
 // add fetch polyfill
 import fetch from "node-fetch";
+import { Crypto } from "@peculiar/webcrypto";
+
 if (!globalThis.fetch) {
   globalThis.fetch = fetch;
   // globalThis.Headers = Headers;
   // globalThis.Request = Request;
   // globalThis.Response = Response;
+}
+
+if (!globalThis.crypto) {
+  globalThis.crypto = new Crypto();
 }
