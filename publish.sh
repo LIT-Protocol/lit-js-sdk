7f9d739a59510113ed8dc3a1d4521dc6052c4afc
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-06-21 12:11:00 -0400

    better publish script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@
 yarn versionBump
 RESULT=$(cat src/version.js | grep -E -o "'.*'")
 VERSION="${RESULT:1:${#RESULT}-2}" # strip the 's at the beginning and end
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 ./gen_docs.sh
 
@@ -10,13 +11,20 @@ yarn build
 yarn buildWeb
 yarn buildNode
 
-if [ $# -eq 0 ]
-  then
+case $BRANCH in
+  "main")
     yarn publish --new-version $VERSION
-else
-    yarn publish --new-version $VERSION --tag $1
-fi
+    ;;
 
+  "beta")
+    yarn publish --new-version $VERSION --tag beta
+    ;;
+
+  *)
+    echo -n "Invalid branch.  Only main and beta supported for publishing."
+    exit
+    ;;
+esac
 
 
 echo "Don't forget to git push to get this running on the netlify cdn"
